a963b17cff9da798474ec2942460a11cc8086967
Your Name <abeniiidereje@gmail.com>
2025-07-09 17:42:58 +0300

    Update on the card validation function component "typScript"

--- src/components/getCardValue.tsx
+++ src/components/getCardValue.tsx
@@ -1,10 +1,12 @@
 
 
- function getCardValue(card) {
-    const faceCardMap = { A: 1, J: 11, Q: 12, K: 13 };
-    const value = card.match(/\d+|[JQKA]/)[0];
-    return faceCardMap[value] || parseInt(value);
+function getCardValue(card: string): number {
+    const faceCardMap: { [key: string]: number } = { A: 1, J: 11, Q: 12, K: 13 };
+    const match = card.match(/\d+|[JQKA]/); // directly on the string
+    const value = match ? match[0] : '0';
+
+    return faceCardMap[value] ?? parseInt(value);
 }
 
 
- export default getCardValue;
+export default getCardValue;
